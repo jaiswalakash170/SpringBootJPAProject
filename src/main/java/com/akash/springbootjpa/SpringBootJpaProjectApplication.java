90412c3f8bef5e28bcd26e1bd6bd1c27cee6b696
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-06-28 15:14:48 +0530

    Initial Commit - Spring Boot JPA-H2 Web App Project

---
+++ src/main/java/com/akash/springbootjpa/SpringBootJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akash.springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/springbootjpa/SpringBootJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
