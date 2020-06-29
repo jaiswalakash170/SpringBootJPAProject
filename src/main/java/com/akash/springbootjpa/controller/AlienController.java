614a1eef8123ef91b9aaa30c38d9d16ef6a46c6c
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-06-29 19:36:24 +0530

    Added updateAlienName updateAlienTech deleteAlien and viewAllAliens requests

--- src/main/java/com/akash/springbootjpa/controller/AlienController.java
+++ src/main/java/com/akash/springbootjpa/controller/AlienController.java
@@ -1,5 +1,7 @@
 package com.akash.springbootjpa.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,46 @@ public class AlienController {
 		ModelAndView mv = new ModelAndView("showAlien.jsp");
 		Alien alien = repo.findById(aid).orElse(new Alien());
 		mv.addObject(alien);
+		return mv;
+	}
+	
+	@RequestMapping("/updateAlienName")
+	public String updateAlienName(@RequestParam() int aid, @RequestParam() String aname)
+	{
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		alien.setAname(aname);
+		repo.save(alien);
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/updateAlienTech")
+	public String updateAlienTech(@RequestParam() int aid, @RequestParam() String tech)
+	{
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		alien.setTech(tech);
+		repo.save(alien);
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/deleteAlien")
+	public String deleteAlien(@RequestParam() int aid)
+	{
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		repo.delete(alien);
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/viewAllAliens")
+	public ModelAndView viewAllAliens()
+	{
+		ModelAndView mv = new ModelAndView("showAllAlien.jsp");
+		Iterable<Alien> itAliens = repo.findAll();
+		ArrayList<Alien> aliens = new ArrayList<Alien>();
+		for(Alien a : itAliens)
+		{
+			aliens.add(a);
+		}
+		mv.addObject("listOfAliens", aliens);
 		return mv;
 	}
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -8,13 +8,41 @@
 </head>
 <body>
 	<form action="addAlien">
+		<h1>Add Alien</h1>
+		<input type="text" name="aid"><br>
+		<input type="text" name="aname"><br>
+		<input type="text" name="tech"><br>
+		<input type="submit">
+	</form>
+	
+	<form action="getAlien">
+		<h1>View Alien Information</h1>
+		<input type="text" name="aid"><br>
+		<input type="submit">
+	</form>
+	
+	<form action="updateAlienName">
+		<h1>Update Alien Name</h1>
 		<input type="text" name="aid"><br>
 		<input type="text" name="aname"><br>
 		<input type="submit">
 	</form>
 	
-	<form action="getAlien">
+	<form action="updateAlienTech">
+		<h1>Update Alien Technology</h1>
 		<input type="text" name="aid"><br>
+		<input type="text" name="tech"><br>
+		<input type="submit">
+	</form>
+	
+	<form action="deleteAlien">
+		<h1>Delete Alien</h1>
+		<input type="text" name="aid"><br>
+		<input type="submit">
+	</form>
+	
+	<form action="viewAllAliens">
+		<h1>View All Aliens</h1>
 		<input type="submit">
 	</form>
 </body>
