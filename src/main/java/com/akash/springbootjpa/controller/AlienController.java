6d8478684fdaaa8aeddc79c3f31d59a609e108b0
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-06-30 10:41:57 +0530

    Removed unused import statement

--- src/main/java/com/akash/springbootjpa/controller/AlienController.java
+++ src/main/java/com/akash/springbootjpa/controller/AlienController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
